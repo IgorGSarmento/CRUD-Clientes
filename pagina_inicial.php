3540d61ab02067e61c4cfc67be5e77d2e270359c
IgorGSarmento <igor_gss@hotmail.com>
2017-08-04 16:06:35 -0300

    pagina_inicial

--- pagina_inicial.php
+++ pagina_inicial.php
@@ -7,12 +7,13 @@
 
         <body>
 		<h1>Pagina Inicial</h1>
-		
+
                 <form>
-			<input type = "button" value = "Adicionar Cliente" onclick = "location. href = 'add_clientes.php'"><br>
-			<input type = "button" value = "Atualizar Cliente" onclick = "location. href = 'att_clientes.php'"><br>
-			<input type = "button" value = "Consultar Cliente" onclick = "location. href = 'cons_clientes.php'"><br>
-			<input type = "button" value = "Deletar Cliente" onclick = "location. href = 'del_clientes.php'"><br>
+			<h3>Opcões</h3>
+			<input type = "button" value = "Adicionar Cliente" onclick = "location. href = 'add_clientes.php'"><br><br>
+			<input type = "button" value = "Atualizar Cliente" onclick = "location. href = 'att_clientes.php'"><br><br>
+			<input type = "button" value = "Consultar Cliente" onclick = "location. href = 'cons_clientes.php'"><br><br>
+			<input type = "button" value = "Deletar Cliente" onclick = "location. href = 'del_clientes.php'"><br><br>
                 </form>
         </body>
 </html>
